ee0102231ffa9b147a535c33db0a93766e5688d7
Renan Loureiro <renanpl47@gmail.com>
2021-12-25 11:42:15 -0300

    config theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
+import { ThemeProvider } from "styled-components"
+
 import { Dashboard } from "./src/components/Dashboard"
 
+import theme from "./src/global/styles/theme"
+
 export default function App() {
-  return <Dashboard />
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -4,5 +4,5 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background: #999;
+  background: ${({ theme }) => theme.colors.primary};
 `
