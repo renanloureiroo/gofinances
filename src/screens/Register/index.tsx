7bb6de56462b21a15518cb06cfe6157f54da35a7
Renan Loureiro <renanpl47@gmail.com>
2021-12-26 16:50:20 -0300

    screen register

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container, Header, Title } from "./styles"
+
+export const Register = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastrar</Title>
+      </Header>
+    </Container>
+  )
+}
