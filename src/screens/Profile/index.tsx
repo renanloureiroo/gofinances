d638d92cd7b1931fe2ad39acbb6646b9852fae48
Renan Loureiro <renanpl47@gmail.com>
2022-01-28 00:25:12 -0300

    test Profile screen

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Button, Text, TextInput, View } from "react-native"
+
+export const Profile = () => {
+  return (
+    <View>
+      <Text>Perfil</Text>
+
+      <TextInput placeholder="Nome" autoCorrect={false} />
+      <TextInput placeholder="Sobrenome" />
+
+      <Button title="Salvar" onPress={() => {}} />
+    </View>
+  )
+}
